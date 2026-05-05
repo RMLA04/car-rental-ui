ff6bed312bfde09282482bab329bd92271a56e42
RMLA04 <adolat.gharibshoeva_2028@ucentralasia.org>
2026-05-05 16:23:23 +0600

    Update db.py

--- db.py
+++ db.py
@@ -5,4 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(os.getenv("DATABASE_URL"))
+    return psycopg2.connect(
+        os.getenv("DATABASE_URL"),
+        sslmode="require"
+    )
